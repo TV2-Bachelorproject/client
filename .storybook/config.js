29bb984c0859d9261d5d2528f57045de59885d03
NJohansen <niclas.w.j@hotmail.dk>
2020-03-31 23:30:46 +0200

    config.js not needed anymore

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { addDecorator, configure } from '@storybook/react';
-
-import StylesDecorator from './styles-decorator';
-
-addDecorator(StylesDecorator);
-
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/../src/components/**/', true, /\.stories\.js$/), module);
