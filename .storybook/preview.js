578a6a8160ddf2d72a5e265547a3a41fa8d64ae1
NJohansen <niclas.w.j@hotmail.dk>
2020-04-27 00:22:08 +0200

    storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,19 @@
 // .storybook/preview.js
+import { addParameters } from "@storybook/react";
+import "../src/index.css";
+import theme from "./theme";
 
-import '../src/index.css';
-
+addParameters({
+  options: {
+    isFullScreen: false,
+    showNav: true,
+    showPanel: true,
+    panelPosition: "right",
+    sortStoriesByKind: false,
+    sidebarAnimations: true,
+    selectedPanel: undefined,
+    enableShortcuts: true,
+    theme: theme,
+    showRoots: true,
+  },
+});
---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "dark",
+  brandTitle: "Credits",
+  brandUrl: "https://tv2.dk",
+  // eslint-disable-next-line max-len
+  fontBase:
+    'AlrightSansLT,-apple-system,".SFNSText-Regular","San Francisco",BlinkMacSystemFont,"Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue","Lucida Grande","Arial",sans-serif',
+});
