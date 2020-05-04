56f103527faab4d5ed6380c16125b8ccc1efbe66
NJohansen <niclas.w.j@hotmail.dk>
2020-05-04 14:46:26 +0200

    get program with graphql

--- src/api/program.js
+++ src/api/program.js
@@ -24,4 +24,29 @@ export default {
     const data = await res.json();
     return data || [];
   },
+  getProgram: async (id) => {
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    let getProgramQuery = `{
+          program(id:${id}){
+            id
+            programId
+            title
+            teaser
+            description
+          }
+        }
+          `;
+    let requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify({ query: getProgramQuery }),
+      redirect: "follow",
+    };
+
+    let res = await fetch("http://localhost:3000/graphql", requestOptions);
+
+    const data = await res.json();
+    return data || [];
+  },
 };
